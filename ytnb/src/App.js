5d5c7b73bfc2e18abf5fbcb12d6ab2d46342967f
sahaycodes <sahay281620@gmail.com>
2023-07-30 19:54:55 +0530

    added note-takin functionality

--- ytnb/src/App.js
+++ ytnb/src/App.js
@@ -1,10 +1,12 @@
-import logo from './components/assests/YTNB.png';
+import React from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import './App.css';
 import { Navbar1 } from './components/NavBar.js';
 import {Banner} from './components/Banner.js'
 import {Library} from './components/Library';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import {YourNotes} from './components/YourNotes';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 
 function App() {
@@ -12,6 +14,7 @@ function App() {
     <div className="App">
       <Navbar1/>
       <Banner />
+      <YourNotes />
       <Library />
      
     </div>
---
+++ ytnb/src/components/YourNotes.js
@@ -0,0 +1,85 @@
+import React, {useState} from 'react';
+import './YourNotes.css';
+const YourNotes = () => {
+
+    const [notes,setNotes]=useState([]);
+    const [newNote , setNewNote]=useState('');
+    const [editingIndex,setEditingIndex] = useState(-1);
+
+    const handleNoteChange=(e) =>{
+        setNewNote(e.target.value);
+    };
+
+    const addNote =() => {
+        if (newNote.trim() !== ' '){
+            if(editingIndex !== -1){
+                const updatedNotes=[...notes];
+                updatedNotes[editingIndex]={
+                    note:newNote,
+                    time: notes[editingIndex].time || new Date().toLocaleDateString(),
+                };//updating existing notes
+                setNotes(updatedNotes);
+                setEditingIndex(-1);
+            }else{
+                setNotes([...notes,{note:newNote,time:new Date().toLocaleTimeString()}]);//adding new note , append notes
+            }   
+            setNewNote(''); 
+        }
+    };
+
+    const editNote = (index) => {
+        setNewNote(notes[index]);
+        setEditingIndex(index);
+    };
+
+    const cancelEdit = () =>{
+        setNewNote('');
+        setEditingIndex(-1);
+    };
+
+    const deleteNote = (index) =>{
+        const updatedNotes=notes.filter((note,i) => i !== index);
+        setNotes(updatedNotes);
+        setEditingIndex(-1);
+    };
+
+    return(
+        <div className='your-notes-container'>
+            <h2>Your Notes</h2>
+            <div className='note-form'>
+                <input
+                type="text"
+                value={newNote}
+                onChange={handleNoteChange}
+                placeholder="Write your note..."
+                />
+                {editingIndex !== -1 ? (
+                    <React.Fragment>
+                        <button onClick={addNote} className="update-btn">Update Note</button>
+                        <button onClick={cancelEdit} className='cancel-btn'>Cancel Edit</button>
+                
+                    </React.Fragment>
+                ):(
+                <button onClick={addNote} className='add-btn'>Add Notes</button>
+                )}
+
+                </div>
+            {notes.length===0?(
+                <p>No notes yet. Start taking notes!</p>
+            ):(
+                <ul className='note-list'>
+                    {notes.map((note,index)=>(
+                        <li key={index} className='note-item'>
+                            {note.note}
+                            <p>Created At: {note.time.toLocaleString()}</p>
+                            <button onClick={() => editNote(index)} className='edit-btn'><i className='fas fa-pencil-alt'></i></button>
+                            <button onClick={() => deleteNote(index)} className='delete-btn'><i className='fas fa-trash-alt'></i></button>
+                        </li>
+                    ))}
+                </ul>
+            )}
+        </div>
+    );
+};
+
+export  {YourNotes} ;
