36b6b26d0fbd9265c903d652edaff0ecd8481dc1
sahaycodes <sahay281620@gmail.com>
2023-08-03 00:51:29 +0530

    private study has some issues .....we will fix it

--- ytnb/src/App.js
+++ ytnb/src/App.js
@@ -1,29 +1,36 @@
 import React from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import { Navbar1 } from './components/NavBar.js';
-import {Banner} from './components/Banner.js'
-import {Library} from './components/Library';
+import { Banner } from './components/Banner.js';
+import { Library } from './components/Library';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {YourNotes} from './components/YourNotes';
+import { YourNotes } from './components/YourNotes';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import { firebaseConfig } from './firebaseConfig';
 import { initializeApp } from 'firebase/app';
 import { StudyRoom } from './components/StudyRoom.js';
+import { PrivateStudy } from './components/PrivateStudy';
 
-const app=initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
 function App() {
   return (
-    <div className="App">
-      <Navbar1/>
-      <Banner />
-      <YourNotes />
-      <StudyRoom />
-      <Library />
-     
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar1 />
+          <Route path="/" exact component={PrivateStudy} />
+          <Route path="/video-chat/:roomCode" component={StudyRoom} />
+            <Banner />
+            <YourNotes />
+            <StudyRoom />
+            <Library />
+  
+      
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
