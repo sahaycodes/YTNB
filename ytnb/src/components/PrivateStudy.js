ce97b793f362e725b869c4f398d6fb12f50a5280
sahaycodes <sahay281620@gmail.com>
2023-08-03 22:45:20 +0530

    add in

---
+++ ytnb/src/components/PrivateStudy.js
@@ -0,0 +1,34 @@
+import React, {useState} from 'react';
+import { useHistory } from 'react-router-dom';
+
+
+const PrivateStudy = () => {
+    const [roomCode,setRoomCode] = useState('');
+    const history = useHistory();
+
+    const generateRoomCode = () => {
+        //generating a random and unique room code 
+        const code = Math.random().toString(36).substring(2,8).toUpperCase();
+        setRoomCode(code);
+    };
+    const handleStartStudy = () => {
+        //redirect to video chat page with generated room code
+        history.push(`/video-chat/${roomCode}`);
+    };
+
+    return(
+        <div>
+            <h2>Create a Private Study</h2>
+            <button onClick={generateRoomCode}>Room Code</button>
+            <br/>
+            {roomCode && (
+                <div>
+                    <p>Your Unique Code: {roomCode}</p>
+                    <button onClick={handleStartStudy}>Start StudyJam</button>
+                    </div>
+            )}
+        </div>
+    );
+};
+
+export {PrivateStudy};
